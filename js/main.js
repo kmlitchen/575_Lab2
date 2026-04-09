fb7609e6d798281613db0e0c0196b4c3d06e9b0b
kmlitchen <litchen@wisc.edu>
2026-04-09 16:43:18 -0500

    week 10 part 1

--- js/main.js
+++ js/main.js
@@ -1,57 +1,135 @@
-// Activity 9 ~
+// Activity 10 ~
 
-// execute fx on load:
-window.onload = setMap();
+// wrap the whole thing up for safe keeping:
+(function () { 
 
-// wrapped fx for map:
-function setMap() {
-    // map frame: 
-    var width = 960,
-        height = 460;
+	// define attributes in pseudo-global scope:
+	var attrArray = ["Rate_All","Rate_WT","Ratio_WT_ST","Rate_BK","Ratio_BK_ST","Ratio_BK_WT"]; 
+	var expressed = attrArray[0]; // set default to first attribute
 
-    // map svg container:
-    var map = d3
-        .select("body")
-        .append("svg")
-        .attr("class", "map")
-        .attr("width", width)
-        .attr("height", height);
+    // execute fx on load:
+    window.onload = setMap();
 
-    // set projection (albers equal area conic for contiguous US)
-    var projection = d3
-        .geoAlbers()
-        .center([-16, 40])
-        .rotate([85.5, 2.5, 3.1])
-        .parallels([8.41, 47.50])
-        .scale(900)
-        .translate([width / 2, height / 2]);
+    // make the map:
+    function setMap() {
+        // map frame: 
+        var width = 960,
+            height = 460;
 
-    var path = d3.geoPath().projection(projection);
+        // map svg container:
+        var map = d3
+            .select("body")
+            .append("svg")
+            .attr("class", "map")
+            .attr("width", width)
+            .attr("height", height);
 
-    // promise it's coming so asynchronous data loading doesn't make it angry:
-    var promises = [
-        d3.csv("data/incarceration.csv"),
-        d3.json("data/usStates.topojson"),
-    ];
-    Promise.all(promises).then(callback);
-    // callback fx once the gang's all here:
-    function callback(data) {
-        var csvData = data[0],
-            statesData = data[1];
-            //console.log(csvData);
-            //console.log(statesData);
+        // set projection (albers equal area conic for contiguous US):
+        var projection = d3
+            .geoAlbers()
+            .center([-16, 40])
+            .rotate([85.5, 2.5, 3.1])
+            .parallels([8.41, 47.50])
+            .scale(900)
+            .translate([width / 2, height / 2]);
 
-        // translate topojson:
-        // not sure what my plan is for AK and HI yet
-        var States = topojson.feature(statesData, statesData.objects.usStates);
-        //console.log(States);
+        var path = d3.geoPath().projection(projection);
+
+        // promise it's coming so asynchronous data loading doesn't make it angry:
+        var promises = [
+            d3.csv("data/incarceration.csv"),
+            d3.json("data/usStates.topojson"),
+        ];
+        Promise.all(promises).then(callback);
+        // callback fx once the gang's all here:
+        function callback(data) {
+            var csvData = data[0],
+                statesData = data[1];
+                console.log(csvData);
+                console.log(statesData);
+
+            // translate topojson:
+            // still not sure what my plan is for AK and HI yet
+            var States = topojson.feature(statesData, statesData.objects.usStates).features;
+            console.log(States);
             
-        // voila -> put states on map
-        var states = map
-            .append("path")
-            .datum(States)
-            .attr("class", "us")
-            .attr("d", path);
+            // calling fx below:
+            States = joinData(States, csvData);
+            var colorScale = makeColorScale(csvData);  
+            setEnumerationUnits(States, map, path, colorScale);
+        }
     }
 
-}
+    // join attribute data + geojson:
+    function joinData(States, csvData) {
+		// loop through csv, for each item define primary key:
+		for (var i = 0; i < csvData.length; i++) {
+			var csvState = csvData[i]; // current state
+			var csvKey = csvState.St_Abbr; // CSV primary key
+
+			// loop through geojson, for each set primary key: 
+			for (var a = 0; a < States.length; a++) {
+				var geojsonProps = States[a].properties; // current state geojson properties
+				var geojsonKey = geojsonProps.state_abbr; // geojson primary key
+
+				// if keys match, assign csv value to geojson properties object:
+				if (geojsonKey == csvKey) {
+					attrArray.forEach(function (attr) {
+						var val = parseFloat(csvState[attr]); // get csv attribute value
+						geojsonProps[attr] = val; // assign attribute and value to geojson properties
+					});
+				}
+			}
+		}
+		return States;
+	}
+
+    // make color scale:
+	function makeColorScale(data){
+		var colorClasses = [
+			"#fee5d9",
+			"#fcbba1",
+			"#fc9272",
+			"#fb6a4a",
+			"#de2d26",
+            "#a50f15",
+		];
+	
+		// color scale generator:
+        var colorScale = d3.scaleQuantile()
+            .range(colorClasses);
+
+        //build two-value array of minimum and maximum expressed attribute values
+        var minmax = [
+            d3.min(data, function(d) { return parseFloat(d[expressed]); }),
+            d3.max(data, function(d) { return parseFloat(d[expressed]); })
+        ];
+        //assign two-value array as scale domain
+        colorScale.domain(minmax);
+
+        return colorScale;
+};
+
+	function setEnumerationUnits(States, map, path, colorScale) {
+		// voila -> put states on map + fill by attribute
+		var states = map
+			.selectAll(".state")
+			.data(States)
+			.enter()
+			.append("path")
+			.attr("class", function (d) {
+				return "state " + d.properties.St_abbr;
+			})
+			.attr("d", path)
+			.style("fill", function (d) {
+				//check to make sure a data value exists, if not set color to gray
+				var value = d.properties[expressed];            
+				if(value) {            	
+					return colorScale(d.properties[expressed]);            
+				} else {            	
+					return "#ccc";            
+				}    
+			})
+	}
+
+})();
